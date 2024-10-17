2bd43544daf1f658a7908ed42b9eb2f3b8d90896
rafhaelguerra <rafhael.war@gmail.com>
2024-10-17 12:53:27 -0300

    lendo conversas no click ok

---
+++ content.js
@@ -0,0 +1,45 @@
+
+watchMsgs = function () {
+    const msgs  = document.querySelectorAll('._amjv._aotl')
+    console.log('lendo chat')
+    const div_array = [...msgs];
+    
+    div_array.forEach(div => {
+        console.log(div)
+        console.log(div.textContent)
+    });
+    
+}
+
+teste = function (el) {
+    console.log('teste de click', el)
+}
+
+
+//lista de conversas
+
+// buscando elemento para click
+const timer = setInterval(()=>{
+
+    // add bt header
+    const header = document.querySelector('.x1qlqyl8.x1pd3egz.xcgk4ki');
+
+    if(header){        
+        clearInterval(timer);
+
+        const button = document.createElement('button');
+        button.innerHTML = 'reading...';
+        button.classList.add('bt-reading');
+
+        button.addEventListener('click', () => {
+            watchMsgs();
+        });
+
+        header.appendChild(button);
+
+    }
+
+
+
+},1500)
+
